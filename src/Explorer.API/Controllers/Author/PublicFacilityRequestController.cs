ccf289052af2555f0f335957cd11b8304393606c
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2024-03-25 12:53:38 +0100

    fix: Changing url

--- src/Explorer.API/Controllers/Author/PublicFacilityRequestController.cs
+++ src/Explorer.API/Controllers/Author/PublicFacilityRequestController.cs
@@ -36,7 +36,7 @@ namespace Explorer.API.Controllers.Author
             request.AuthorName = _userService.Get(long.Parse(identity.FindFirst("id").Value)).Value.Username;
             request.AuthorPicture = _userService.Get(long.Parse(identity.FindFirst("id").Value)).Value.ProfilePicture;
             using StringContent jsonContent = new(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
-            using HttpResponseMessage response = await client.PostAsync("http://localhost:88/publicFacilityRequest/create", jsonContent);
+            using HttpResponseMessage response = await client.PostAsync("http://host.docker.internal:88/publicFacilityRequest/create", jsonContent);
             var jsonResponse = await response.Content.ReadAsStringAsync();
             return CreateResponse(jsonResponse.ToResult());
         }
