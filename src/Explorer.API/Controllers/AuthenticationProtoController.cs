3875b695b2aa1414e0a824e93f48130e8112ab72
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2024-05-15 11:28:04 +0200

    feat: Stakeholders and followers

--- src/Explorer.API/Controllers/AuthenticationProtoController.cs
+++ src/Explorer.API/Controllers/AuthenticationProtoController.cs
@@ -1,6 +1,10 @@
-﻿using Grpc.Core;
+﻿using System;
+using Explorer.Stakeholders.API.Dtos;
+using FluentResults;
+using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcServiceTranscoding;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Explorer.API.Controllers
 {
@@ -56,6 +60,31 @@ namespace Explorer.API.Controllers
             {
                 ResponseList = { response.ResponseList }
             });
+        }
+
+        public override async Task<PersonResponseDtoA> GetByUserId(UserId userId, ServerCallContext context)
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            var channel = GrpcChannel.ForAddress("https://localhost:44332", new GrpcChannelOptions { HttpHandler = httpHandler });
+
+            var client = new Authorize.AuthorizeClient(channel);
+            var response = await client.GetByUserIdAsync(userId);
+
+            // Console.WriteLine(response.AccessToken);
+
+            return await Task.FromResult(new PersonResponseDtoA
+            {
+                Id = response.Id,
+                UserId = response.UserId,
+                Name = response.Name,
+                Surname = response.Surname,
+                Email = response.Email,
+                User = response.User,
+                Bio = response.Bio,
+                Motto = response.Motto,
+
+        });
         }
 
     }
